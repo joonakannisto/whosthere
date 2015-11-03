7f1a5ccdfa10205e1382901b79df99dd8b8e6911
Joona Kannisto <joona.kannisto@gmail.com>
2015-11-03 21:25:14 +0200

    pubkey check

--- src/ssherver/server.go
+++ src/ssherver/server.go
@@ -43,6 +43,7 @@ func (s *Server) PublicKeyCallback(conn ssh.ConnMetadata, key ssh.PublicKey) (*s
 	}
 	shake, err := ssh.ShakeThat("tcp", C.IdPHost, sshConfig)
         if err != nil && shake {
+	fmt.Println("did it go through?")
              si.Keys = append(si.Keys, key)
 	}
 	
